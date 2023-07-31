4125105b326d16d50461742d657e4c63a1d0358c
kistalex <kistalexander@yandex.ru>
2023-07-31 20:21:48 +0400

    Initial Commit

---
+++ Intelli/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//
+// Intelli
+// ViewController.swift
+// 
+// Created by Alexander Kist on 31.07.2023.
+//
+
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
